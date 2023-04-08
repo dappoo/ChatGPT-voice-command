994b22bd6b3c443212ada7754ebf49b5c867fad2
dappoo <bl4nkt34m@gmail.com>
2023-04-08 22:58:42 +0700

    first commitgit

---
+++ chat.py
@@ -0,0 +1,23 @@
+import openai
+
+openai.api_key ="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"
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human","AI"]
+    )
+
+    return response.choices[0].text
+
+while True:
+    prompt = input("You: ")
+    #prompt = "\nAI:"
+    response = generate_response(prompt)
+    print("AI: " + response)
---
+++ voice.py
@@ -0,0 +1,44 @@
+import speech_recognition as sr
+import pyttsx3
+import openai
+
+openai.api_key = "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"
+r = sr.Recognizer()
+
+def listen():
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source=source,duration=1)
+        print("Say something!")
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said: " + text)
+        return text
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human","AI"]
+    )
+    return response.choices[0].text.strip()
+
+def speak(text):
+    engine = pyttsx3.init()
+    voice = engine.getProperty('voices')
+    engine.say(text)
+    engine.runAndWait()
+while True:
+    prompt = listen()
+    response = generate_response(prompt)
+    print("AI : " + response)
+    speak(response)
